00bfdb70539b99e775db478f173e92100b9290be
Samuele Niccolai <71394602+SamueleNiccolai@users.noreply.github.com>
2026-08-21 21:49:59 +0200

    Add: three-integer-sum - submission-9

---
+++ Data Structures & Algorithms/three-integer-sum/submission-9.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Set<List<Integer>> result = new HashSet<>();
+        int i = 0;
+        int len = nums.length;
+        Arrays.sort(nums);
+        while (i < len - 2) {
+            int j = i + 1;
+            int k = len - 1;
+            while (j < k) {
+                if ((nums[i] + nums[j] + nums[k]) == 0) {
+                    List<Integer> list = List.of(nums[i], nums[j], nums[k]);
+                    result.add(list);
+                }
+                if(nums[j] + nums[k] < 0 -nums[i])
+                    j++;
+                else
+                    k--;
+            }
+            i++;
+        }
+        return new ArrayList<>(result);
+    }
+}
