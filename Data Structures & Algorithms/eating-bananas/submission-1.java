27443e39a623d36c8e2c63640e883d906f82d595
Samuele Niccolai <71394602+SamueleNiccolai@users.noreply.github.com>
2026-08-26 23:02:54 +0200

    Add: eating-bananas - submission-1

---
+++ Data Structures & Algorithms/eating-bananas/submission-1.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int base = 1;
+        Arrays.sort(piles);
+        int max = piles[piles.length - 1];
+        // il numero di ore puo andare da un minimo di piles.length se k = max value
+        // a un massimo equivalente al prodotto di tutti i elementi se k = 1;
+        Integer minK = null;
+        while (base <= max) {
+            int k = (max + base) / 2;
+            int hours = 0;
+            for (int i = 0; i < piles.length; i++) {
+                int resto = piles[i] % k == 0 ? 0 : 1;
+                hours = hours + Math.max(1, piles[i] / k + resto);
+            }
+            if (hours > h) {
+                base = k + 1;
+            } else {
+                max = k - 1;
+                minK = minK != null ? Math.min(minK, k) : k;
+            }
+        }
+        return minK;
+    }
+}
