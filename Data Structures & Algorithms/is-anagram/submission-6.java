ea3c7596b5b1e3528e8529dfd4f6f20b2219a83f
Samuele Niccolai <71394602+SamueleNiccolai@users.noreply.github.com>
2026-08-30 05:34:08 +0200

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        /***
+         * se s e t hanno lengt() diversa sicuramente non sono un anagramma
+         * 3 Modi:
+         * - Sorto le stringhe e poi controllo se sono uguali -> C: O(nlogn), S: O(1)? non so se il
+         * sorting dichiara una struttura n di appoggio o no
+         * - Faccio una mappa <Carattere, Occorrenze> dove aggiungo i caratteri
+         *   della prima stringa e sottraggo i caratteri della seconda.
+         *   una volta sottratto se trovo un carattere con occorrenza != 0
+         *   allora non è un anagramma. -> C: O(n), S:O(n)
+         * - s e t contengono solo lettere dell'alfabeto inglese in minuscolo
+         *   quindi posso dichiarare 2 array di lunghezza 26 contare le occorrenze
+         *   e poi controllare se gli array sono uguali. C: O(n), S:O(1)
+         */
+        if (s.length() != t.length())
+            return false;
+        int[] a1 = new int[26];
+        int[] a2 = new int[26];
+        // Sicuramente hanno la stessa length 
+        // quindi scorrendone uno posso scorrere l'altro
+        for (int i = 0; i < s.length(); i++) {
+            a1[s.charAt(i)-'a']++;
+            a2[t.charAt(i)-'a']++;
+        }
+        return Arrays.equals(a1,a2);
+    }
+}
