c8608985321fef0dff97548739b106c0d9999c16
Samuele Niccolai <71394602+SamueleNiccolai@users.noreply.github.com>
2026-08-30 12:26:17 +0200

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isValid(String s) {
+        // Map close to open
+        Map<Character, Character> map = new HashMap<>();
+        map.put(')', '(');
+        map.put('}', '{');
+        map.put(']', '[');
+        // Declare a Stack
+        Deque stack = new ArrayDeque<>();
+        for (char c : s.toCharArray()) {
+            // se è un carattere di chiusura
+            if (map.containsKey(c)) {
+                // se ho la corrispondente apertura
+                if (stack.peek() == map.get(c)) {
+                    stack.pop();
+                    continue;
+                } else {
+                    return false;
+                }
+            } else {
+                // se ho un carattere di apertura
+                stack.push(c);
+            }
+        }
+        if(!stack.isEmpty()) return false;
+        return true;
+    }
+}
